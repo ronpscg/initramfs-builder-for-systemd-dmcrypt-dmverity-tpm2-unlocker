1aed02cf5e0930b69087cc156d02d022aa80cd5b
Ron Munitz <ron@thepscg.com>
2025-12-21 15:12:28 +0100

    fedora: modified to --no-hostonly added systemd-cryptenroll and removed the unnecessary inclusion of cryptsetup which is already included.

--- workdir/fedora/build.sh
+++ workdir/fedora/build.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 OUTPUT_FILE=$PWD/initrd.img
 
+: ${MORE_INCLUDES=""}
+
+if [ "$(grep ^ID= /etc/os-release  | cut -d= -f2)" = "fedora" ] ; then
+	# The idea is to allow auto enrollment also from initramfs (if one wishes to do so, via initramfs hooks that run *before* the typical systemd-cryptgenerator et. al services)
+	echo "Adding systemd-cryptenroll for Fedora" 
+	MORE_INCLUDES="--include /usr/sbin/systemd-cryptenroll /usr/sbin/systemd-cryptenroll"
+fi
+
 dracut --force \
-       --hostonly \
-       --no-kernel \
-       --include "/usr/lib/cryptsetup" "/usr/lib/cryptsetup" \
-       $OUTPUT_FILE
+	--no-hostonly 	\
+	--no-kernel 	\
+	$MORE_INCLUDES 	\
+	$OUTPUT_FILE
